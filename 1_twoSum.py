8741aa8b6d4b01a9da24f7388851b19451a5d4f5
cuijiaxun <cuijiaxun_sjtu@163.com>
2021-01-02 15:11:00 -0600

    1

--- 1_twoSum.py
+++ 1_twoSum.py
@@ -5,26 +5,30 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        """Solution 1"""
+        #32ms, 13.4mb
         num1 = nums
         num2 = {}
         for i in range(len(num1)):
             current = target - num1[i]
+
+            if num1[i] in num2.keys():
+                index_1 = i
+                index_2 = num2[num1[i]].pop()
+                break
+                
             if current not in num2.keys():
                 num2[current] = set()
             num2[current].add(i)
-        #print(num2)
-        for i in range(len(num1)):
-            if num1[i] in num2.keys():
-                if i not in num2[num1[i]]:
-                    index_1 = i
-                    index_2 = num2[num1[i]].pop()
-                    break
-                elif len(num2[num1[i]])>1:
-                    num2[num1[i]].remove(i)
-                    print(num2)
-                    index_1 = i
-                    index_2 = num2[num1[i]].pop()
-                    break
-                else:
-                    continue
+
         return [index_1,index_2]
+        """Solution2
+        #36ms, 13.6mb
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
+        """

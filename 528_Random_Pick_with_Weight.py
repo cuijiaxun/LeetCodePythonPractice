6d62a93e2c81406ccef1298fa5c20b2dea61eccf
cuijiaxun <cuijiaxun_sjtu@163.com>
2021-01-03 22:57:47 -0600

    528

---
+++ 528_Random_Pick_with_Weight.py
@@ -0,0 +1,29 @@
+import random
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.w = w
+        self.w_sum = []
+        self.sum = 0
+        for i in range(len(w)):
+            self.sum += w[i]
+            self.w_sum.append(self.sum)
+
+    def pickIndex(self) -> int:
+        if len(self.w) ==1: return 0
+        choice = random.randint(0, self.sum-1)
+        index = self.binSearch(self.w_sum, choice, 0, len(self.w)-1)
+        #print(choice, index)
+        return index
+    
+    def binSearch(self, array, target, low, high):
+        if low == 0:
+            if array[low]> target: return 0
+        if high-low <=1:
+            return high
+        mid = (low+high)//2
+        if array[mid]<=target:
+            return self.binSearch(array, target, mid, high)
+        else:
+            return self.binSearch(array, target, low, mid)
+        return None

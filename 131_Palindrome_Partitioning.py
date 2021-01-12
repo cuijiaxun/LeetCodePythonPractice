7ffefd0eddf84b430d924d0774d074dac422c35d
cuijiaxun <cuijiaxun_sjtu@163.com>
2021-01-12 17:13:32 -0600

    Interviewed Google Today

---
+++ 131_Palindrome_Partitioning.py
@@ -0,0 +1,21 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        if not s: return []
+        n = len(s)
+        def if_palindrome(sub):
+            return sub == sub[::-1]
+        
+        res = []
+    
+        def dfs(start, path):
+            if start == n:
+                res.append(path.copy())
+                return
+            for i in range(start, n):
+                if if_palindrome(s[start: i+1]):
+                    path.append(s[start: i+1])
+                    dfs(i+1, path)
+                    path.pop()
+              
+        dfs(0, [])
+        return res
